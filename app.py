fb09cd86ac9bf624d817100efd158dcd7bcf0e14
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-14 17:47:43 +0200

    Subida inicial

---
+++ app.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+
+class Interface(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.grid()
+
+        self.fnameLabel = Label(master, text="Url del video")
+        self.fnameLabel.grid()
+
+        self.fnameEntry = StringVar()
+        self.fnameEntry = Entry(textvariable=self.fnameEntry)
+        self.fnameEntry.grid()
+
+        def buttonClick():
+            print("You pressed Submit!")
+            print(self.fnameEntry.get() + ", you clicked the button!")
+
+        self.submitButton = Button(master, text="Descargar", command=buttonClick)
+        self.submitButton.grid()
+
+
+class Logger(object):
+    def debug (self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+if __name__ == "__main__":
+    guiFrame = Interface()
+    guiFrame.mainloop()
